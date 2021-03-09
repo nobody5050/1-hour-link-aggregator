67e9a31862b5cdac7024daaa9d3bd3274337e261
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2021-03-09 11:41:00 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyACGMUH95QAxKGK-G7GKOCpr3N8OBUo6WM",
+    authDomain: "testliftion.firebaseapp.com",
+    databaseURL: "https://testliftion.firebaseio.com",
+    projectId: "testliftion",
+    storageBucket: "testliftion.appspot.com",
+    messagingSenderId: "584922554416",
+    appId: "1:584922554416:web:a5ba70b065c69166"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
